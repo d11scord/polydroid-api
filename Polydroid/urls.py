2a331f7cdd225cdff94829a96537c91edd56bb4c
d11scord <juliakuzkina1@gmail.com>
2020-03-29 22:04:45 +0300

    Add search endpoint again

--- Polydroid/urls.py
+++ Polydroid/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
@@ -69,6 +70,9 @@ urlpatterns = [
 
     # Расписания
     path('schedule/group/<int:pk>/', lesson_group, name='lesson-group'),
+
+    # Поиск
+    url(r'^search-objects/$', SearchViewSet.as_view({'get': 'list'})),
 
     # Администрирование
     path('admin/', admin.site.urls),
