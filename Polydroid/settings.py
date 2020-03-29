aae235a02dd957d433acf03d8bb88abee756d280
d11scord <juliakuzkina1@gmail.com>
2020-03-29 21:31:39 +0300

    Change permissions for Heroku

--- Polydroid/settings.py
+++ Polydroid/settings.py
@@ -55,7 +55,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
-            'rest_framework.permissions.IsAdminUser',
+            # 'rest_framework.permissions.IsAdminUser',
         ]
 }
 
