d1d6e4572b2d2bb1537ebda664fc0f0f5711ca7b
d11scord <juliakuzkina1@gmail.com>
2020-05-21 19:34:03 +0300

    Disabla parser

--- Polydroid/settings.py
+++ Polydroid/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    # 'request',
 
     'schedule',
-    'schedule_parser',
+    # 'schedule_parser',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'request.middleware.RequestMiddleware'
 ]
 
 REST_FRAMEWORK = {
