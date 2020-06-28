63ce9f6a41a97b6807306272c32e83933dc7cdda
d11scord <juliakuzkina1@gmail.com>
2020-06-28 19:34:08 +0300

    Add django-request and LessonType to admin site

--- Polydroid/settings.py
+++ Polydroid/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'request',
+    'request',
 
     'schedule',
     'schedule_parser',
